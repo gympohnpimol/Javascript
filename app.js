cacc185fdefc2e9db48d5e7be85d9e91d15392f5
gympohnpimol <gympohnpimol@gmail.com>
2021-02-23 03:34:33 +0900

    test

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("Hello Gym")
+console.log("Hello Gym")
+console.log("Hello Gym")
+//alert("Hello World")
+
+var name = "DinoYoooo"
+console.log(name)
+
+name = "Pop"
+console.log(name)
+
+var firstname = "gym"
+var lastname = "dinogym"
+console.log(firstname)
+console.log(lastname)
+console.log(firstname+" "+lastname)
+var age = 20
+alert(typeof age)
+
+{
+    var x=200
+    let y=5
+}
+console.log(x)
+
+let n = prompt("Input your name!")
+console.log("Hello "+n)
